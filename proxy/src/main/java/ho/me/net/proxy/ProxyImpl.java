92a3de86304e477b3badc1a4ae1b1907bc59b1ee
Shivan.Naidoo <Shivan.Naidoo@interfront.co.za>
2017-04-01 09:05:58 +0200

    wip

---
+++ proxy/src/main/java/ho/me/net/proxy/ProxyImpl.java
@@ -0,0 +1,12 @@
+package ho.me.net.proxy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ProxyImpl {
+
+
+
+}
---
+++ proxy/src/main/java/ho/me/net/proxy/RouteFilter.java
@@ -0,0 +1,72 @@
+package ho.me.net.proxy;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Slf4j
+@Component
+public class RouteFilter extends ZuulFilter {
+
+    @Value("${unauthorized.url.redirect:http://blog.the-coffee-beans.info}")
+    private String redirect;
+
+    @Override
+    public String filterType() {
+        return "route";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    private boolean isAuthorizedRequest(HttpServletRequest request) {
+        return true;
+    }
+
+    private void setRouteHost(RequestContext ctx) throws MalformedURLException {
+
+        String urlS = ctx.getRequest().getRequestURL().toString();
+        URL url = new URL(urlS);
+        String protocol = url.getProtocol();
+        String rootHost = url.getHost();
+        int port = url.getPort();
+        String portS = (port > 0 ? ":" + port : "");
+        ctx.setRouteHost(new URL(protocol + "://" + rootHost + portS));
+
+    }
+
+    public Object run() {
+        log(" -- Intercepting request");
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+
+        try {
+            if (isAuthorizedRequest(ctx.getRequest())) {
+                setRouteHost(ctx);
+            } else {
+
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void log(String message) {
+        System.out.println(message);
+    }
+
+}
