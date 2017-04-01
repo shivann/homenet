ff9f4bd22b51908d816e314b1fc79dd737c261bc
Shivan.Naidoo <Shivan.Naidoo@interfront.co.za>
2017-04-01 09:44:25 +0200

    wip

--- proxy/src/main/java/ho/me/net/proxy/ProxyImpl.java
+++ proxy/src/main/java/ho/me/net/proxy/ProxyImpl.java
@@ -1,12 +1,16 @@
 package ho.me.net.proxy;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
 public class ProxyImpl {
 
-
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(ProxyImpl.class).bannerMode(Banner.Mode.OFF).run(args);
+    }
 
 }
